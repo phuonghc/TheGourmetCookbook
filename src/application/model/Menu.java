c609be7eb233e875278071e4a4aa043c2e76f2ee
sayontaniray <pek684@my.utsa.edu>
2019-04-18 17:05:28 -0500

    Added javadocs for Menu related classes.

--- src/application/model/Menu.java
+++ src/application/model/Menu.java
@@ -2,22 +2,45 @@ package application.model;
 
 import java.util.ArrayList;
 
+/**
+ * This class contains attributes and behaviors related to object Menu.
+ * 
+ * @author Sayontani Ray (pek684)
+ * UTSA CS 3443 - Team Project
+ * Spring 2019
+ */
 public class Menu {
 	
+	// Attributes or properties of a Menu
 	private int totalResults;
 	private ArrayList<MenuItem> results;
-
-	public ArrayList<MenuItem> getResults() {
-		return results;
-	}
-	public void setResults(ArrayList<MenuItem> results) {
-		this.results = results;
-	}
+	
+	/**
+	 * @return the totalResults
+	 */
 	public int getTotalResults() {
 		return totalResults;
 	}
+	
+	/**
+	 * @param totalResults the totalResults to set
+	 */
 	public void setTotalResults(int totalResults) {
 		this.totalResults = totalResults;
+	}
+	
+	/**
+	 * @return the results
+	 */
+	public ArrayList<MenuItem> getResults() {
+		return results;
+	}
+	
+	/**
+	 * @param results the results to set
+	 */
+	public void setResults(ArrayList<MenuItem> results) {
+		this.results = results;
 	}
 	
 }
